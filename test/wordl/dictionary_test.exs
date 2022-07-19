78c3f7c31e54ab9edd6e83bc55a7aa27a312e19c
GuilAFT <guilaft@riseup.net>
2022-07-18 23:27:12 -0300

    test(dictionary): Add test for accented words

--- test/wordl/dictionary_test.exs
+++ test/wordl/dictionary_test.exs
@@ -17,6 +17,13 @@ defmodule DictionaryTest do
       refute Dictionary.has_word?(dict_name, "olá")
     end
 
+    test "has_word/2 ignores accents" do
+      Dictionary.start_link(name: "spanish", path: "assets/dictionaries/Spanish.txt")
+
+      assert Dictionary.has_word?("spanish", "ábaco")
+      assert Dictionary.has_word?("spanish", "abaco")
+    end
+
     test "random_word/2 returns a random word with a given length", %{dict_name: dict_name} do
       random_word = Dictionary.random_word(dict_name, 5)
       assert Dictionary.has_word?(dict_name, random_word)
